6ea286f70d1c155baad31c4fdf672163ff3264e8
Hoodoo42 <create@hoodooverse.ca>
2022-08-29 17:26:45 -0600

    did not need to loop through user information to dynamically display in their profile. Was able to just send the response info into data and statically fill it into the value attributes using v-bind. Now to get the edit button to allow the user to edit their profile information

--- src/components/RestaurantProfile.vue
+++ src/components/RestaurantProfile.vue
@@ -3,36 +3,72 @@
     <button @click="access_profile">Profile</button>
 
     <div v-if="profile_accessed">
-      <label for="name">Name:</label>
-      <input ref="name" type="name" name="name" v-model="profile_name" />
+      <article>
+        <label for="name">Name:</label>
+        <input ref="name" type="name" name="name" :value="profile[`name`]" />
 
-      <label for="email">Email:</label>
-      <input ref="email" type="email" name="email" v-model="profile_email" /><br />
+        <label for="email">Email:</label>
+        <input
+          ref="email"
+          type="email"
+          name="email"
+          :value="profile[`email`]"
+        /><br />
 
-      <!-- string in the form of ###-###-#### -->
-      <label for="phone_number">Phone Number:</label>
-      <input ref="phone_number" type="tel" name="phone_number" v-model="profile_number" />
-      <br />
+        <!-- string in the form of ###-###-#### -->
+        <label for="phone_number">Phone Number:</label>
+        <input
+          ref="phone_number"
+          type="tel"
+          name="phone_number"
+          :value="profile[`phone_number`]"
+        />
+        <br />
 
-      <label for="bio">Bio:</label>
-      <input ref="bio" type="text" name="bio" v-model="profile_bio" /> <br />
+        <label for="bio">Bio:</label>
+        <input ref="bio" type="text" name="bio" :value="profile[`bio`]" />
+        <br />
 
-      <label for="address">Address:</label>
-      <input ref="address" type="text" name="address" v-model="profile_address" /> <br />
+        <label for="address">Address:</label>
+        <input
+          ref="address"
+          type="text"
+          name="address"
+          :value="profile[`address`]"
+        />
+        <br />
 
-      <!-- must be Calgary, Vancouver, or Toronto -->
-      <label for="city">City:</label>
-      <input ref="city" type="text" name="city" v-model="profile_city" /> <br />
+        <!-- must be Calgary, Vancouver, or Toronto -->
+        <label for="city">City:</label>
+        <input ref="city" type="text" name="city" :value="profile[`city`]" />
+        <br />
 
-      <label for="profile_pic">Profile Picture:</label>
-      <input ref="profile_pic" type="url" name="profile_pic" />
+        <label for="profile_pic">Profile Picture:</label>
+        <input
+          ref="profile_pic"
+          type="url"
+          name="profile_pic"
+          :value="profile[`profile_pic`]"
+        />
 
-      <label for="banner">Banner:</label>
-      <input ref="banner" type="url" name="banner" />
+        <label for="banner">Banner:</label>
+        <input
+          ref="banner"
+          type="url"
+          name="banner"
+          :value="profile[`banner`]"
+        />
 
-      <label for="password">Password:</label>
-      <input ref="password" type="password" name="password" v-model="profile_password" />
-      <br />
+        <label for="password">Password:</label>
+        <input
+          ref="password"
+          type="password"
+          name="password"
+          :value="profile[`password`]"
+        />
+        <br />
+      </article>
+      <!-- <button @click="edit_button">Edit</button> -->
     </div>
   </div>
 </template>
@@ -45,14 +81,7 @@ export default {
     return {
       profile_accessed: undefined,
 
-    // profile:[]
-      profile_name: '',
-      profile_email: '',
-      profile_address: '',
-      profile_number: '',
-      profile_bio: '',
-      profile_city: '',
-      profile_password: '',
+      profile: [],
     };
   },
 
@@ -74,21 +103,7 @@ export default {
         })
         .then((res) => {
           res;
-// I should probably put this into some sort of loop perhaps
-
-        //   for(let i=0; i<res[`data`][0].length; i++){
-        //     this.profile = res[`data`][0];
-        //   }
-
-
-          this.profile_name = res[`data`][0][`name`]
-          this.profile_email = res[`data`][0][`email`]
-          this.profile_address = res[`data`][0][`address`]
-          this.profile_number = res[`data`][0][`phone_number`]
-          this.profile_bio = res[`data`][0][`bio`]
-          this.profile_city = res[`data`][0][`city`]
-
-          
+          this.profile = res[`data`][0];
         })
         .catch((err) => {
           err;
