c957e58677294bd55134977291e05dccd83d79f8
Hoodoo42 <create@hoodooverse.ca>
2022-09-07 12:26:08 -0600

    set up log out functionality for client side

---
+++ src/components/ClientLogout.vue
@@ -0,0 +1,33 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import cookies from "vue-cookies";
+import axios from "axios";
+export default {
+  created() {
+    let access_token = cookies.get(`client_token`);
+
+    axios
+      .request({
+        url: `https://innotechfoodie.ml/api/client-login`,
+        headers: {
+          "x-api-key": "ZKmQmvzJKfctNlIVXzeU",
+          token: access_token,
+        },
+        method: `DELETE`,
+      })
+      .then((res) => {
+        res;
+        this.$router.push(`/`);
+      })
+      .catch((err) => {
+        err;
+      });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/ClientHome.vue
+++ src/views/ClientHome.vue
@@ -1,27 +1,36 @@
 <template>
   <div>
-      
     <button @click="open_profile">Profile</button>
-
     <client-profile v-if="profile"></client-profile>
+
+    <button @click="open_logout">Log Out</button>
+    <client-logout v-if="logging_out"></client-logout>
+
   </div>
 </template>
 
 <script>
 import ClientProfile from "@/components/ClientProfile.vue";
+import ClientLogout from "@/components/ClientLogout.vue"
 export default {
   components: {
     ClientProfile,
+    ClientLogout
   },
   data() {
     return {
       profile: undefined,
+      logging_out: undefined
     };
   },
   methods: {
     open_profile() {
       this.profile = true;
+
     },
+    open_logout(){
+      this.logging_out = true;
+    }
   },
 };
 </script>
